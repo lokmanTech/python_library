4f81ea5203ad1fd4f5c035220956b3b9be74238d
Lokman Hakim <142791901+lokmanTech@users.noreply.github.com>
2024-05-21 16:49:44 +0800

    add two integers & update readme

---
+++ python3/add_two_Integers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
+    
+# Example 1
+solution = Solution()
+num1 = 12
+num2 = 5
+print(solution.sum(num1, num2)) #Output: 17
+
+# Example 2
+num1 = -10
+num2 = 4
+print(solution.sum(num1, num2)) #Output: -6
